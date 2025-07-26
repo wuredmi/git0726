cfc408048d6f658686c103e0efce300ac8ea971f
wuredmi9t <wuredmi9t@gmail.com>
2025-07-26 16:27:06 +0800

    更新起迄日期

--- 1.py
+++ 1.py
@@ -7,7 +7,8 @@ print("Hello Python!!")
 print(f"今天日期{dt.datetime.today().strftime("%Y%m%d")}")
 
 url="https://api.cnyes.com/media/api/v1/newslist/category/headline"  #連結
-payload= {"page":1,"limit":30,"startAt":1752645542,"endAt":1753509542}  #參數
+#payload= {"page":1,"limit":30,"startAt":1752645542,"endAt":1753509542}  #參數
+payload= {"page":1,"limit":30,"startAt":int((dt.datetime.today() - dt.timedelta(days=11)).timestamp()),"endAt":int(dt.datetime.today().timestamp())}  #參數
 res=requests.get(url, params = payload)  #連線鉅亨網
 jd= json.loads(res.text)  #解析json轉成dict
 df= pd.DataFrame(jd["items"]["data"])  #取出新聞資料
